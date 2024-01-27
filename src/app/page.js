6e87e5eac556ae52abfcb57e677348c14b74088e
UndineA <anazuss@inbox.lv>
2024-01-27 16:51:45 +0200

    JAN-25 Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Footer from "@/components/Footer/Footer";
+
 export default function Home() {
   return (
-    
     <main>
       Shadow platform project
+      <Footer />
     </main>
-  )
+  );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "@mui/material/List";
+import FooterItem from "./FooterItem";
+
+function Footer() {
+  return (
+    <List>
+      <FooterItem />
+    </List>
+  );
+}
+
+export default Footer;
