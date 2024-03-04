1ae922e27d185500575b97939ac594d0deecb75f
UndineA <anazuss@inbox.lv>
2024-03-04 22:01:14 +0200

    More styling

--- src/components/Registration/Registration.config.js
+++ src/components/Registration/Registration.config.js
@@ -1,12 +1,16 @@
 export const RegistrationCards = [
   {
-    icon: "Ikona ēnotājam",
+    icon: "group",
+    iconLink:
+      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0",
     name: "Ēnotājs",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     link: "/registration/shadow",
   },
   {
-    icon: "Ikona profesionālim",
+    icon: "work",
+    iconLink:
+      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0",
     name: "Profesionālis vai uzņēmums",
     text: "Dolorum facilis culpa incidunt. Perferendis deleniti tenetur amet nobis quisquam officia.",
     link: "/registration/professional",
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -6,11 +6,9 @@ import Link from "next/link";
 const Registration = () => {
   return (
     <>
-      <div className={styles.reg_header} key="reg-header">
-        Logo
-      </div>
+      <div className={styles.reg_header} key="reg-header"></div>
       <div className={styles.reg_body} key="reg-body">
-        <span class="material-symbols-outlined">
+        <span className="material-symbols-outlined">
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
@@ -19,22 +17,29 @@ const Registration = () => {
         </span>
         <Link href="/">Atpakaļ</Link>
         <div className={styles.reg_content}>
-          <h1>Pievienojies kā ēnotājs vai kā profesionālis vai uzņēmums?</h1>
-          <h3>
+          <h1 className={styles.title}>
+            Pievienojies kā ēnotājs vai kā profesionālis vai uzņēmums?
+          </h1>
+          <h2 className={styles.subtitle}>
             Autem sunt ratione asperiores odit nobis consequuntur quo sequi.
-          </h3>
+          </h2>
           <div className={styles.reg_cards} key="reg-cards">
             {RegistrationCards.map((item) => (
               <div className={styles.card} key={item.link}>
-                <div>{item.icon}</div>
-                <h3>{item.name}</h3>
-                <p>{item.text}</p>
-                <button href={item.link}>Reģistrēties</button>
+                <span className="material-symbols-outlined">
+                  <link rel="stylesheet" href={item.iconLink} />
+                  {item.icon}
+                </span>
+                <h3 className={styles.card_title}>{item.name}</h3>
+                <p className={styles.card_text}>{item.text}</p>
+                <button className={styles.button} href={item.link}>
+                  Reģistrēties
+                </button>
               </div>
             ))}
           </div>
-          <p>
-            Esi jau reģistrējies TryWork? <a>Ienākt</a>
+          <p className={styles.login}>
+            Esi jau reģistrējies TryWork? <Link href="">Ienākt</Link>
           </p>
         </div>
       </div>
