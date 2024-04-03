7d774ba26c41e122b01ce3c3fff0dbd8a5a1c219
MenteeVelga51 <amazone.velga@gmail.com>
2024-04-03 13:12:52 +0300

    Login form
    Login form with 4 errors I don't see where

---
+++ src/components/Login form/Login form.jsx
@@ -0,0 +1,14 @@
+<head>Pierakstīšanās forma</head>
+<html lang="lv">
+<div class="container">
+    <h2>Pierakstīšanās forma</h2>
+    <label for="username"><b>Lietotājvārds</b></label>
+    <input type="text" placeholder="Enter Username" id="username" required>
+
+    <label for="password"><b>Parole</b></label>
+    <input type="password" placeholder="Enter Password" id="password" required>
+
+    <button onclick="login()">Pierakstīties</button>
+</div>
+</body>
+</html>
---
+++ src/components/Registration success page/Registration success page.jsx
@@ -0,0 +1,15 @@
+<><head>Registration success page</head><script>
+    baseurl="<><head>Registration success page</head><script>
+    baseurl="image-20240222-102323.png"
+    function buildURL(item)
+    {item.href = baseurl + window.location.href};
+    return true;
+</script><body>
+        <a onclick="return buildURL(this)" href="">registration / success</a>
+    </body></>"
+    function buildURL(item)
+    {item.href = baseurl + window.location.href};
+    return true;
+</script><body>
+        <a onclick="return buildURL(this)" href="">registration / success</a>
+    </body></>
