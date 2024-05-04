b2c81321b0ad190190bb768fbe507cfc9e2883e3
UndineA <anazuss@inbox.lv>
2024-05-04 23:58:59 +0300

    Color, text etc. changes

--- src/app/registration/page.js
+++ src/app/registration/page.js
@@ -1,30 +1,49 @@
 import Registration from "@/components/Registration/Registration";
 import Image from "next/image";
 import Link from "next/link";
-import BackArrow from "@/components/Registration/img/back_arrow.png";
+import BackArrow from "@/components/Registration/img/arrow.svg";
 import styles from "@/components/Registration/Registration.module.scss";
+import { Button } from "@mui/material";
 
 export default function RegistrationPage() {
   return (
-    <>
-      <div className={styles.reg_body}>
+    <div className={styles.registration_body}>
+      <Button
+        href="/"
+        sx={{
+          textTransform: "none",
+          display: "flex",
+          gap: "8px",
+          padding: "4px",
+          width: "93px",
+          height: "30px",
+          color: "#347DCA",
+          borderRadius: "100px",
+          letterSpacing: "0.46px",
+          lineHeight: "22px",
+          fontWeight: "500",
+          fontSize: "14px",
+          "&:hover": {
+            backgroundColor: "#d6e5f4",
+          },
+        }}
+      >
         <Image src={BackArrow} alt="Back arrow icon" />
-        <Link href="/" className={styles.back}>
-          Atpakaļ
-        </Link>
-        <div className={styles.reg_content}>
-          <h1 className={styles.title}>
-            Pievienojies kā ēnotājs vai kā profesionālis vai uzņēmums?
-          </h1>
-          <h2 className={styles.subtitle}>
-            Autem sunt ratione asperiores odit nobis consequuntur quo sequi.
-          </h2>
-          <Registration />
-          <p className={styles.login}>
-            Esi jau reģistrējies TryWork? <Link href="">Ienākt</Link>
-          </p>
-        </div>
+        <div>Atpakaļ</div>
+      </Button>
+
+      <div className={styles.registration_body_content}>
+        <h1 className={styles.registration_body_content_title}>
+          Pievienojies kā Ēnotājs vai kā Ēnu devējs
+        </h1>
+        <Registration />
+        <p className={styles.registration_body_content_login}>
+          Esi jau reģistrējies TryWork?
+          <Link href="" className={styles.registration_body_content_login_link}>
+            Ienākt
+          </Link>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -2,18 +2,45 @@
 import { RegistrationCards } from "./Registration.config";
 import styles from "./Registration.module.scss";
 import Image from "next/image";
+import { Button } from "@mui/material";
 
 const Registration = () => {
   return (
-    <div className={styles.reg_cards}>
+    <div className={styles.registration_body_content_cards}>
       {RegistrationCards.map((item) => (
-        <div className={styles.card} key={item.link}>
+        <div
+          className={styles.registration_body_content_cards_card}
+          key={item.link}
+        >
           <Image src={item.icon} alt={item.alt} />
-          <h3 className={styles.card_title}>{item.name}</h3>
-          <p className={styles.card_text}>{item.text}</p>
-          <button className={styles.button} href={item.link}>
-            Reģistrēties
-          </button>
+          <h3
+            className={styles.registration_body_content_cards_card_title}
+            style={{ color: item.color }}
+          >
+            {item.name}
+          </h3>
+          <p className={styles.registration_body_content_cards_card_text}>
+            {item.text}
+          </p>
+          <Button
+            href={item.link}
+            sx={{
+              textTransform: "none",
+              backgroundColor: item.color,
+              color: "#ffffff",
+              padding: "8px 22px",
+              borderRadius: "40px",
+              fontSize: "15px",
+              fontWeight: "500",
+              lineHeight: "26px",
+              letterSpacing: "0.46px",
+              "&:hover": {
+                backgroundColor: item.background,
+              },
+            }}
+          >
+            Reģistrējies
+          </Button>
         </div>
       ))}
     </div>
